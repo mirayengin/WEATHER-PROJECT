48b32c36e39ce1c5d0ad22fb03277be9fc4a727a
erhan <erhanserkansabriye@gmail.com>
2022-09-28 17:06:41 +0300

    weather son hali

--- weather.js
+++ weather.js
@@ -1,5 +1,6 @@
 const input = document.querySelector("input");
 const button = document.querySelector("button");
+const wrongDiv = document.querySelector(".wrong");
 let isError = false;
 
 //* api'den veri çekmek için
@@ -14,7 +15,7 @@ const getWeather = async (cityName) => {
       // throw new Error(`Something went wrong. Failure Code :${res.status}`);
     }
     const data = await res.json();
-
+    
     console.log(data);
     renderWeather(data);
   } catch (error) {
@@ -22,13 +23,24 @@ const getWeather = async (cityName) => {
   }
 };
 
+
 //* değerleri yazdırmak için
 const weatherListDiv = document.querySelector(".row");
 const renderWeather = (item) => {
-  // const newsList = document.querySelector(".newsList");
+  console.log(cities);
+  console.log(item);
+  console.log(input.innerText);
   if (isError) {
-    // weatherListDiv.innerHTML = `<h2>City is wrong</h2>`;
-    // return;
+    wrongDiv.innerHTML = `<h2>City name writes wrong</h2>`;
+    cities = cities.filter((a) => a !== cities[cities.length -1])
+    console.log(cities);
+    
+  }
+
+  if (cities.length == 4) {
+    weatherListDiv.firstElementChild.remove();
+    cities.shift();
+    console.log(cities);
   }
   const { name, main, weather } = item;
 
@@ -37,7 +49,7 @@ const renderWeather = (item) => {
       <ul class="list-group list-group-flush">
         <li class="list-group-item item1">${name.replace("Province", "")} ${Math.round(main.temp)}<sup>°C</sup></li>
         
-        <li class="list-group-item item4">${Math.round(main.temp_min)}°C / ${Math.round(main.temp_max)}°C</li>
+        <li class="list-group-item item4">Min ${Math.round(main.temp_min)}°C /Max ${Math.round(main.temp_max)}°C</li>
         <img class="img " src="http://openweathermap.org/img/w/${
           weather[0].icon
         }.png" class="card-img-top" alt="icon" />        
@@ -60,23 +72,28 @@ const renderWeather = (item) => {
 };
 
 
-
 let cities = [];
+
+
 const aciklama = document.querySelector(".aciklama")
 //* buton click için
 button.addEventListener("click", () => {
+  isError = false
   if(cities.includes(input.value.toLowerCase())){
-    aciklama.innerHTML += `<h2>You know the weather for ${input.value.toUpperCase()}</h2>`;
+    aciklama.innerHTML += `<h2>You know the weather for <span class="danger">${input.value.toUpperCase()}</span></h2>`;
+    wrongDiv.innerHTML = `<h2></h2>`;
   }else{
   cities.push(input.value.toLowerCase())
   getWeather(input.value);
-  aciklama.innerHTML = `<h2> </h2>`;
+    aciklama.innerHTML = `<h2> </h2>`;
+    wrongDiv.innerHTML = `<h2></h2>`;
 }
   input.value = "";
 });
 
 //* enter tuşu aktif olması için
 input.addEventListener("keydown", (e) => {
+  isError = false
   e.key === "Enter" && button.click();
   // input.value = "";
 });
